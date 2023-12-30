9870d5b9e333b09be780a102be1459ac56967acb
Tatum Allen <82176414+tatumalllen@users.noreply.github.com>
2023-12-29 18:33:15 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+function onPageLoad() {
+    console.log('Page loaded successfully!');
+}
+
+// Call the onPageLoad function when the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', onPageLoad);
